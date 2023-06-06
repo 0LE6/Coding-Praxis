7f4a6ad19605711e67b3352cb83be2e2ed747231
0LE6 <135649528+0LE6@users.noreply.github.com>
2023-06-07 01:24:43 +0200

    Create 20_Valid_Parenthesis.cs

---
+++ LeetCode/20_Valid_Parenthesis.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public bool IsValid(string s) {
+        while(s.Contains("()") || s.Contains("[]") || s.Contains("{}"))
+            s = s.Replace("()","").Replace("[]","").Replace("{}","");
+        return s.Length == 0;
+    }
+}
