0be970a5190b160e2a7a38f20e58f5c4568eaa04
0LE6 <135649528+0LE6@users.noreply.github.com>
2023-06-07 01:58:41 +0200

    Update 20_Valid_Parenthesis.cs

--- LeetCode/20_Valid_Parenthesis.cs
+++ LeetCode/20_Valid_Parenthesis.cs
@@ -1,3 +1,30 @@
+// Improved version using Stack (More efficient)
+
+public class Solution {
+    public bool IsValid(string s) {
+        Stack<char> cStack = new Stack<char>();
+        bool valid = true;
+        foreach (char c in s){
+            if (c.Equals('(') || c.Equals('[') || c.Equals('{'))
+                cStack.Push(c);
+            else if (c.Equals(')')){
+                if (cStack.Count != 0 && cStack.Peek().Equals('(')) cStack.Pop();
+                else valid = false;
+            }
+            else if (c.Equals(']')){
+                if (cStack.Count != 0 && cStack.Peek().Equals('[')) cStack.Pop();
+                else valid = false;
+            }
+            else if (c.Equals('}')){
+                if (cStack.Count != 0 && cStack.Peek().Equals('{')) cStack.Pop();
+                else valid = false;
+            }
+        }
+        return cStack.Count == 0 && valid;
+    }
+}
+
+/* Poor first version (Valid anyways):
 public class Solution {
     public bool IsValid(string s) {
         while(s.Contains("()") || s.Contains("[]") || s.Contains("{}"))
@@ -5,5 +32,4 @@ public class Solution {
         return s.Length == 0;
     }
 }
-
-// Poor first version (Valid anyways)
+*/
