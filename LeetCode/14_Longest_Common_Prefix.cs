ebc55fc9fbec5ec5c91bce7c62cca0b20e4d75b7
0LE6 <135649528+0LE6@users.noreply.github.com>
2023-06-07 17:34:42 +0200

    Create 14_Longest_Common_Prefix.cs

---
+++ LeetCode/14_Longest_Common_Prefix.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        string firstStr = strs[0];
+        string str = "";
+        if (strs.Length == 0) str = "";
+        int i = 0;
+        bool valid = true;
+        while (i < firstStr.Length && valid)
+        {
+            char curChar = firstStr[i];
+
+            for (int j = 1; j < strs.Length; j++)
+            {
+                string nextStr = strs[j];
+
+                if (i >= nextStr.Length || nextStr[i] != curChar)
+                    valid = false;
+            }
+            if (valid) str += curChar;
+            i++;
+        }
+        return str;
+    }
+}
+
+// Runtime 88 ms | Beats 97.27%
