9bb5bea565584150d77b9a8c1c9302c0386f363d
0LE6 <135649528+0LE6@users.noreply.github.com>
2023-06-15 22:10:17 +0200

    Update 0119_Pascal's_Triangle_II.cs

--- LeetCode/0119_Pascal's_Triangle_II.cs
+++ LeetCode/0119_Pascal's_Triangle_II.cs
@@ -1,19 +1,18 @@
 // Based on my "0118_Pascal's_Triangle" solution
 
 public class Solution {
-    public IList<IList<int>> Generate(int numRows) {
-        IList<IList<int>> pascalsTriangle = new List<IList<int>>();
-
-        for (int i = 0; i < numRows; i++){
-
-            IList<int> row = new List<int>();
+    public IList<int> GetRow(int rowIndex) {
+        IList<int> row = new List<int>();
+        
+        for (int i = 0; i < rowIndex + 1; i++){
+            IList<int> newRow = new List<int>();
 
             for (int j = 0; j <= i; j++){
-                if (j == 0 || j == i) row.Add(1);
-                else row.Add(pascalsTriangle[i - 1][j - 1] + pascalsTriangle[i - 1][j]);
+                if (j == 0 || j == i) newRow.Add(1);
+                else newRow.Add(row[j - 1] + row[j]);
             }
-        pascalsTriangle.Add(row);
+            row = newRow;
         }
-        return pascalsTriangle;
+        return row; 
     }
 }
