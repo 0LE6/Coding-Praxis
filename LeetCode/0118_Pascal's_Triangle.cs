87b862b9e5dc6ea1b692040ade0deff2574b375a
0LE6 <135649528+0LE6@users.noreply.github.com>
2023-06-15 14:19:42 +0200

    Create 0118_Pascal's_Triangle.cs

---
+++ LeetCode/0118_Pascal's_Triangle.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public IList<IList<int>> Generate(int numRows) {
+        IList<IList<int>> pascalsTriangle = new List<IList<int>>();
+
+        for (int i = 0; i < numRows; i++){
+
+            IList<int> row = new List<int>();
+
+            for (int j = 0; j <= i; j++){
+                if (j == 0 || j == i) row.Add(1);
+                else row.Add(pascalsTriangle[i - 1][j - 1] + pascalsTriangle[i - 1][j]);
+            }
+        pascalsTriangle.Add(row);
+        }
+        return pascalsTriangle;
+    }
+}
