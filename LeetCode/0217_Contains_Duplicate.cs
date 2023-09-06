c2184888d4b1b20a14cb11fe2d515d18fe215480
0LE6 <135649528+0LE6@users.noreply.github.com>
2023-09-06 21:08:00 +0200

    Create 0217_Contains_Duplicate.cs

---
+++ LeetCode/0217_Contains_Duplicate.cs
@@ -0,0 +1,16 @@
+// Time 0(n) / Space 0(n)
+public static bool ContainsDuplicate(int[] nums) {
+    HashSet<int> set = new HashSet<int>(nums);
+    return set.Count != nums.Length ? true : false;
+}
+
+// Time 0(n * log(n)) / Space 0(n)
+public static bool ContainsDuplicate2(int[] nums) {
+    Array.Sort(nums);
+    bool twice = false; int i = 1;
+    while (i < nums.Length && !twice) {
+        twice = nums[i - 1] == nums[i];
+        i++;
+    }
+    return twice;
+}
